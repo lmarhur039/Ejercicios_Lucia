94a73f80ad3783db0a09e19203e1afbe94d1ab23
lmarhur039 <lmarhur039@g.educaand.es>
2023-10-10 08:56:44 +0200

    cambios

---
+++ src/Divisible.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Divisible {
+    public static void main(String[] args) {
+
+            int n, d;
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("Introduzca dos enteros:");
+            n = entrada.nextInt();
+            d = entrada.nextInt();
+            if (n%d == 0)
+                System.out.println(n +" es divisible por "+d);
+    }
+
+}
---
+++ src/signonumero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+    class SignoNumero {
+        public static void main(String[] a) {
+            float numero;
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("Introduzca un número real");
+            numero = entrada.nextFloat();
+            // comparar número con cero
+            if (numero > 0)
+                System.out.println(numero + " es mayor que cero");
+            if (numero < 0)
+                System.out.println(numero + " es menor que cero");
+            if (numero == 0)
+                System.out.println(numero + " es igual que cero");
+
+    }
+}
