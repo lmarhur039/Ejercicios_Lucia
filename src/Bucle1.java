859ab70d8a693a475602a68e482762d38506d8f8
lmarhur039 <lmarhur039@g.educaand.es>
2023-10-17 09:14:28 +0200

    cambios

---
+++ src/Bucle1.java
@@ -0,0 +1,14 @@
+public class Bucle1 {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while (contador <= 100){
+            System.out.println("contador:" + contador);
+            contador++; //contador = contador + 1
+        }
+
+        System.out.println("Fin del bucle.");
+    }
+
+}
